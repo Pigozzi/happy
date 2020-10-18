ebdc108b9c28dda6e2caa708a5325fcd3ce2b73b
pigozzi <rafaelpigozzi@gmail.com>
2020-10-18 19:06:24 -0300

    Alteraçõs no backend e frontend

--- backend/src/database/migrations/1602683485820-create_orphanages.ts
+++ backend/src/database/migrations/1602683485820-create_orphanages.ts
@@ -44,7 +44,7 @@ export class createOrphanages1602683485820 implements MigrationInterface {
                 },
                 {
                     name: 'open_on_weekends',
-                    type: 'text',
+                    type: 'boolean',
                     default: false
                 }
             ],
